7e1edfe240d937939cf7219c46876d0d0b608d28
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-05-02 11:08:19 +0100

    api request being handled

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { APP_ID, APP_KEY } from './config/dev.js';
+
 let foodToSearch = null;
 
 function handleRecipeClick() {
@@ -5,10 +7,13 @@ function handleRecipeClick() {
 }
 
 function handleFoodChange() {
-  foodToSearch = document.querySelector("#food-input").value;
+  foodToSearch = document.querySelector('#food-input').value;
 }
 
 async function fetchRecipe(food) {
-  //--- write your code below ---
-  //--- write your code above ---
+  const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const response = await fetch(requestUrl);
+  const jsonResponse = await response.json();
+  const recipe = jsonResponse.hits[0];
 }
+fetchRecipe('potato');
